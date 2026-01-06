67568e5a5c9adbd7283ca082d2a5836a805dc0ab
Lena <ananchenko2306@gmail.com>
2026-01-06 23:24:29 +0200

    initial commit

--- src/main/java/com/task1/Main.java
+++ src/main/java/com/task1/Main.java
@@ -4,8 +4,5 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String [] args){
-        Scanner in = new Scanner(System.in);
-        int number = in.nextInt();
-        System.out.println(number/10 + " " + number%10);
     }
 }
